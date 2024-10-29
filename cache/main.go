da17b7f0f6ddf3ef6103ba852204c8f7f79d8abc
Daniel Peshkov <109922163+DanielPeshkov@users.noreply.github.com>
2024-10-29 14:19:18 -0400

    Cache

---
+++ cache/main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"cache/cache"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"time"
+	"unicode"
+
+	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Server struct {
+	listenAddr string
+	ln         net.Listener
+	quit       chan struct{}
+	cache      cache.Cache
+	queue      *amqp.Channel
+}
+
+func NewServer(listenAddr string) *Server {
+	return &Server{
+		listenAddr: listenAddr,
+		quit:       make(chan struct{}),
+		cache: cache.Cache{
+			Users: make(map[int64]cache.User),
+		},
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.listenAddr)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Server is running")
+	defer ln.Close()
+	s.ln = ln
+
+	// Load cache from DB
+	dbConn, err := net.Dial("tcp", os.Getenv("DB"))
+	if err != nil {
+		return fmt.Errorf("failed to connect to db: %s", err)
+	}
+	s.cache.Load(dbConn)
+	dbConn.Close()
+
+	// Connect to RabbitMQ
+	queue, err := amqp.Dial(os.Getenv("RABBIT"))
+	if err != nil {
+		return fmt.Errorf("failed to connect to queue: %s", err)
+	}
+	defer queue.Close()
+	qch, err := queue.Channel()
+	if err != nil {
+		return fmt.Errorf("failed to connect to queue channel: %s", err)
+	}
+	defer qch.Close()
+	s.queue = qch
+	fmt.Println("Connected to RabbitMQ")
+
+	go s.acceptLoop()
+	<-s.quit
+	return nil
+}
+
+func (s *Server) acceptLoop() {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		fmt.Println("new connection to the server: ", conn.RemoteAddr())
+		go s.readLoop(conn)
+	}
+}
+
+func (s *Server) readLoop(conn net.Conn) {
+	// Create connection to DB microservice
+	dbConn, err := net.Dial("tcp", os.Getenv("DB"))
+	if err != nil {
+		fmt.Println("forward connection error: ", err)
+		return
+	}
+	defer dbConn.Close()
+
+	// Communication loop
+	defer conn.Close()
+	buf1 := make([]byte, 2048)
+	buf2 := make([]byte, 2048)
+	for {
+		// Read gateway request
+		n, err := conn.Read(buf1)
+		if err != nil {
+			if n == 0 {
+				fmt.Println("closing connection to:", conn.RemoteAddr())
+				conn.Close()
+				return
+			}
+			fmt.Println("read error: ", err)
+			continue
+		}
+
+		// Forward request to DB Service
+		msg1 := buf1[:n]
+		data, ts, req, id, reqId := parseRequest(msg1)
+		// Measure DB response time
+		start := time.Now()
+
+		msg2, len := s.cache.Query(dbConn, buf2, data, req, reqId, id)
+		msg2 = msg2[:len]
+		// Measure DB response time
+		elapsed := time.Since(start)
+		conn.Write(msg2)
+
+		// Log response time
+		gateLat := start.UnixMilli() - ts
+		reqLat := elapsed
+		out := fmt.Sprintf("%d, gateway latency: %dms, request latency: %dus", start.Unix(), gateLat, reqLat.Microseconds())
+
+		err = s.queue.Publish("", "logs", false, false, amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(out),
+		})
+		if err != nil {
+			fmt.Println("Error writing to queue: ", err)
+		}
+	}
+}
+
+func parseRequest(msg []byte) ([]byte, int64, []byte, int64, []byte) {
+	i := 0
+	for msg[i] != 35 {
+		i += 1
+	}
+	length := len(msg) - i - 22
+	i += 20
+	ts, err := strconv.ParseInt(string(msg[i:i+13]), 10, 64)
+	i += 14
+	if err != nil {
+		fmt.Println("timestamp error: ", err)
+	}
+	// Request Type extraction for cache validation
+	j := i + 8
+	for msg[j] != '"' {
+		j += 1
+	}
+	req := msg[i+8 : j]
+
+	for msg[j] != ':' {
+		j += 1
+	}
+	j += 1
+	k := j
+	for unicode.IsDigit(rune(msg[k])) {
+		k += 1
+	}
+	id := int64(0)
+	if j != k {
+		id, err = strconv.ParseInt(string(msg[j:k]), 10, 64)
+		if err != nil {
+			id = 0
+		}
+	}
+	reqId := msg[len(msg)-23 : len(msg)-2]
+	data := append([]byte(fmt.Sprintf(`%d#{"pattern":{`, length)), msg[i:]...)
+	return data, ts, req, id, reqId
+}
+
+func main() {
+	godotenv.Load()
+	server := NewServer(os.Getenv("PORT"))
+	server.Start()
+}
