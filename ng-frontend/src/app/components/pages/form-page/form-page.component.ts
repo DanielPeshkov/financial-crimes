8bb5b8cfedcdcac8399c7d17ba14ad07e94bdf2e
Ghub Ghub <rihop6@gmail.com>
2024-11-04 00:34:08 -0500

    Crime type button activation and form type handling in URL

--- ng-frontend/src/app/components/pages/form-page/form-page.component.ts
+++ ng-frontend/src/app/components/pages/form-page/form-page.component.ts
@@ -2,18 +2,19 @@ import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TabComponent } from '../../general/tab/tab.component';
 import { CardComponent } from '../../general/card/card.component';
+import { CommonModule, Location } from '@angular/common';
 
 @Component({
   selector: 'app-form-page',
   standalone: true,
-  imports: [TabComponent, CardComponent],
+  imports: [TabComponent, CardComponent, CommonModule],
   templateUrl: './form-page.component.html',
   styleUrl: './form-page.component.scss'
 })
 export class FormPageComponent {
-  formType: string = "other"
+  formType: string = "other";
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private location: Location) {
     this.formType = this.route.snapshot.params['formtype'];
   }
 
@@ -29,4 +30,10 @@ export class FormPageComponent {
     if (nextIndex < this.tabs.length)
       document.querySelectorAll('app-tab')[nextIndex].scrollIntoView({ behavior: 'smooth' });
   }
+
+  formTypeSelect(type: string) {
+    this.formType = type;
+    this.location.replaceState(type);
+  }
+
 }
