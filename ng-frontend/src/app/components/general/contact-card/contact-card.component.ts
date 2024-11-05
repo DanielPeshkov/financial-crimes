c1c7bf9dd8543e1d1d08ca4672d4ff2981a25080
Ghub Ghub <rihop6@gmail.com>
2024-11-05 01:23:04 -0500

    Finished styling for contact form

--- ng-frontend/src/app/components/general/contact-card/contact-card.component.ts
+++ ng-frontend/src/app/components/general/contact-card/contact-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, inject, input, Input, OnInit, Output } from '@angular/core';
 import { FormService } from '../../../services/form.service';
 import { FormArray, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-card',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact-card.component.html',
   styleUrl: './contact-card.component.scss'
 })
